50af71981934c997f02b57d0b54bd124e2c7f5b8
pauldropz <pauldropz@gmail.com>
2022-09-30 18:44:01 +0100

    navbar.jsx && navbar.css

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import './App.css';
-
+import Header from "./components/header/header";
 
 
 function App () {
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,8 @@
+import "./nav.css";
+
+
+import { AiOutlineHome } from "react-icons/ai"
+import { AiOutlineUser } from "react-icons/ai"
+import { TiGroupOutline } from "react-icons/ti"
+import { BiMessageRoundedDot } from "git"
+import { AiOutlineHome } from "react-icons/ai"
