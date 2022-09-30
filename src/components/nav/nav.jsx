fd13004b8a8130cf276d8e1c54b087b1b38a68b1
pauldropz <pauldropz@gmail.com>
2022-09-30 19:06:38 +0100

    navbar links

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,8 +1,40 @@
 import "./nav.css";
-
-
 import { AiOutlineHome } from "react-icons/ai"
 import { AiOutlineUser } from "react-icons/ai"
 import { TiGroupOutline } from "react-icons/ti"
-import { BiMessageRoundedDot } from "git"
-import { AiOutlineHome } from "react-icons/ai"
+import { BiMessageRoundedDots } from "react-icons/bi"
+import { BsArrowDownCircle } from "react-icons/bs"
+
+
+function Navbar () {
+
+    return (
+        <div className="navigation">
+
+                <a href="#home">
+                        <AiOutLineHome className="icon active-nav" />
+                </a>
+
+                <a href="#home">
+                        <AiOutLineHome className="icon active-nav" />
+                </a>
+
+                <a href="#home">
+                        <TiGroupOutline className="icon active-nav" />
+                </a>
+
+                <a href="#home">
+                        <BiMessageRoundedDots className="icon active-nav" />
+                </a>
+
+                <a href="#home">
+                        <BsArrowDownCircle className="icon active-nav" />
+                </a>
+
+
+
+        </div>
+    )
+}
+
+export default Navbar;
