1d851d375716b2560562fadee689fa964367a451
pauldropz <pauldropz@gmail.com>
2022-09-30 12:28:31 +0100

    keyframes fade-in insert

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import "/header.css";
 function Header () {
         return (
 
-            <div id="home" className="container header-container">
+            <div id="home" className="header-container">
                 <h3>
 
                     <p> I <span>'</span>m</p>
