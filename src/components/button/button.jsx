5e484f3edf199fb1616f8133d7f92ac6a85c85cd
pauldropz <pauldropz@gmail.com>
2022-10-01 10:09:18 +0100

    styling for button-container .btn.pri::after

---
+++ src/components/button/button.jsx
@@ -0,0 +1,20 @@
+import "/button.css";
+
+function Buttons () {
+    return (
+        <div className="button-container">
+
+        <a href="" className="btn pri">
+
+            Learn more
+
+        </a>
+        <a href="#contact" className="btn sec">
+
+        Get in Touch
+        </a>
+        </div>
+
+    );
+}
+export default Buttons;
