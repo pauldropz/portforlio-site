1912043ac1dbced388e7658859be184f00c67de0
pauldropz <pauldropz@gmail.com>
2022-10-01 09:47:49 +0100

    home.jsx function  exported

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -29,11 +29,21 @@ import "/.home.css";
                 <BsMouse className="scroll" />
                 <hr />
 
-                    <h2><span>About Me</span>
+                    <h2><span>About Me</span><br />                
+                        <p>
+                        lorem loremlorkm dfcbwncvhwfew cegwvcetwvfcew cewvtewcte3yfcv3 cv3yt3tc3tc
+                        ctegfceycyece c3ycfgyc3fcyt34f 3fdytd6y4g34 fy4g3yfb43f34f34f
+                        f43fg3yfgy3gf4ygf4yfg4y3 f43yfg4yfg43 fg3yfg3ygf4y3?
+                        </p>
+                              
                     </h2>
 
                 </a>
             </div>
     );
  }
- export default Home;
+ const toggle = document.querySelector (".main-img");
+ toggle.addEventListener ("click", () => {
+     toggle.classList.toggle("active")
+    })
+export default Home;
