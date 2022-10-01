70b4837c8769b9bdb64c453a59b3fa6ab280dd9c
pauldropz <pauldropz@gmail.com>
2022-10-01 07:09:28 +0100

    function home edition

---
+++ src/components/home/home.jsx
@@ -0,0 +1,25 @@
+import "/.home.css";
+
+ import img "../props/img2.jpg";
+ import { BsMouse } from "react-icons/bs";
+
+
+
+
+ function Home () {
+    return (
+            <div id="home" className="container home-container">
+                <div className="logo">
+                    <div className="main-img">
+                        
+
+
+
+                    </div>
+                </div>
+
+
+            </div>
+    );
+ }
+ export default Home;
