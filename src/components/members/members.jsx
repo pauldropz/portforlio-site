7a3c7562c042634d6b7eb2e6b01dab48ffe7a6dd
pauldropz <pauldropz@gmail.com>
2022-10-02 18:33:56 +0100

    members.jsx comp

---
+++ src/components/members/members.jsx
@@ -0,0 +1,80 @@
+import "./members.css";
+
+
+
+const Members= () => {
+    return (
+        <div id="members" className="members-container">
+            <h1 className="member-txt">Members</h1>
+        <div className="member-1">
+                        <div className="member-img"></div>
+                    <div className="member-info">
+                        <h1 className="name">Paul</h1>
+                        <h3 className="position">Leader</h3>
+                        <h4 className="about">Lorem vhfdjvbgv vfjhvnyuaref aklgvjiuponrecyreu cuahgoiebancliweah byuesrvbyreu
+                        viheirvgherv vuirehieur</h4>
+                        <a href="#contact" className="contact-member">
+                            <span>contact</span>
+                        </a>
+                    </div>
+        </div>
+
+        <div className="member-2">
+                        <div className="member-img"></div>
+                    <div className="member-info">
+                        <h1 className="name">Paul</h1>
+                        <h3 className="position">Leader</h3>
+                        <h4 className="about">Lorem vhfdjvbgv vfjhvnyuaref aklgvjiuponrecyreu cuahgoiebancliweah byuesrvbyreu
+                        viheirvgherv vuirehieur</h4>
+                        <a href="#contact" className="contact-member">
+                            <span>contact</span>
+                        </a>
+                    </div>
+        </div>
+
+        <div className="member-3">
+                        <div className="member-img"></div>
+                    <div className="member-info">
+                        <h1 className="name">Paul</h1>
+                        <h3 className="position">Leader</h3>
+                        <h4 className="about">Lorem vhfdjvbgv vfjhvnyuaref aklgvjiuponrecyreu cuahgoiebancliweah byuesrvbyreu
+                        viheirvgherv vuirehieur</h4>
+                        <a href="#contact" className="contact-member">
+                            <span>contact</span>
+                        </a>
+                    </div>
+        </div>
+
+        <div className="member-4">
+                        <div className="member-img"></div>
+                    <div className="member-info">
+                        <h1 className="name">Paul</h1>
+                        <h3 className="position">Leader</h3>
+                        <h4 className="about">Lorem vhfdjvbgv vfjhvnyuaref aklgvjiuponrecyreu cuahgoiebancliweah byuesrvbyreu
+                        viheirvgherv vuirehieur</h4>
+                        <a href="#contact" className="contact-member">
+                            <span>contact</span>
+                        </a>
+                    </div>
+        </div>
+
+        <div className="member-5">
+                        <div className="member-img"></div>
+                    <div className="member-info">
+                        <h1 className="name">Paul</h1>
+                        <h3 className="position">Leader</h3>
+                        <h4 className="about">Lorem vhfdjvbgv vfjhvnyuaref aklgvjiuponrecyreu cuahgoiebancliweah byuesrvbyreu
+                        viheirvgherv vuirehieur</h4>
+                        <a href="#contact" className="contact-member">
+                            <span>contact</span>
+                        </a>
+                    </div>
+        </div>
+
+
+        
+
+        </div>
+    );
+}
+export default Members;
