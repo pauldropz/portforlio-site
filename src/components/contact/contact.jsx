30cfd9a4b2f273cb793bd5fe9031d6d22f9069a4
pauldropz <pauldropz@gmail.com>
2022-10-02 19:41:47 +0100

    function Contact exported

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -12,15 +12,29 @@ function Contact () {
            
                 <div className="contact-links">
 
-                    <a href="#">contact youtube</a>
+                    <a href="#" className="contact youtube">
                         <AiOutlineYoutube className="icon" />
                         <h2>youtube<span>Creative Ambition</span></h2>
+                    </a>
+
+                    <a href="#" className="contact Whatsapp">
+                        <AiOutlineWhatsapp className="icon" />
+                        <h2>Whatsapp<span>+000 000 0000</span></h2>
+                    </a>
+
+                    <a href="#" className="contact Instagram">
+                        <AiOutlineInstagram className="icon" />
+                        <h2>Instagram<span>_Creative _Ambition_</span></h2>
+                    </a>
+
+
                 </div>
         </div>
     )
+
 }
 
-
+export default Contact;
 
 
 
