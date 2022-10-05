3374e9c93f17cb122008b39d045f94570d68832d
pauldropz <pauldropz@gmail.com>
2022-10-05 23:40:07 +0100

    chore: Webcontent changed in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import webContent from './app';
+import App from './app';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <webContent />
+    <App />
   </React.StrictMode>
 );
 
